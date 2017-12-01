5ab2860bef0b084559f9dd940656804604774d7f
L-Cafe <L-Cafe@users.noreply.github.com>
2017-12-01 11:59:01 +0100

    start

---
+++ Minstagram/src/minstagram/Minstagram.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minstagram;
+
+/**
+ *
+ * @author x000
+ */
+public class Minstagram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
